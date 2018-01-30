27c5ae1932211044575af8eaa3df960131c10250
Ibrahim Qadhmani <ibqadamani@gmail.com>
2018-01-30 08:23:44 -0500

    Add files via upload

---
+++ NightmareOfAnimus/NightmareOfAnimus/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Nightmare of Animus")]
+[assembly: AssemblyProduct("NightmareOfAnimus")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyDescription("My first big 2D game and my final project.")]
+[assembly: AssemblyCompany("IQadhmani inc.")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e8406a5a-4fa5-439a-918f-94986d2194dd")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
